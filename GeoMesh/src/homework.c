c82239a3951113b4d0f12b57c8df2ce72bec473d
De Raadt Tristan <tristan.deraadt@student.uclouvain.be>
2025-02-11 15:02:27 +0100

    Added assignment 2 and completed Q1

---
+++ GeoMesh/src/homework.c
@@ -0,0 +1,114 @@
+#include "fem.h"
+
+
+
+
+double geoSize(double x, double y){
+
+    femGeo* theGeometry = geoGetGeometry();
+    
+    double h = theGeometry->h;
+    double x0 = theGeometry->xNotch;
+    double y0 = theGeometry->yNotch;
+    double r0 = theGeometry->rNotch;
+    double h0 = theGeometry->hNotch;
+    double d0 = theGeometry->dNotch;
+  
+    
+    double x1 = theGeometry->xHole;
+    double y1 = theGeometry->yHole;
+    double r1 = theGeometry->rHole;
+    double h1 = theGeometry->hHole;
+    double d1 = theGeometry->dHole;
+
+
+//
+//     A modifier !
+//     
+// Your contribution starts here ....
+//
+    
+    return h;
+    
+//   
+// Your contribution ends here :-)
+//
+
+}
+
+
+#define ___ 0
+
+void geoMeshGenerate() {
+
+    femGeo* theGeometry = geoGetGeometry();
+
+    double w = theGeometry->LxPlate;
+    double h = theGeometry->LyPlate;
+
+    double x = theGeometry->xPlate;
+    double y = theGeometry->yPlate;
+     
+    double x0 = theGeometry->xNotch;
+    double y0 = theGeometry->yNotch;
+    double r0 = theGeometry->rNotch;
+    
+    
+    double x1 = theGeometry->xHole;
+    double y1 = theGeometry->yHole;
+    double r1 = theGeometry->rHole;
+ 
+//
+//  -1- Construction de la géométrie avec OpenCascade
+//      On crée le rectangle
+//      On crée les deux cercles
+//      On soustrait les cercles du rectangle :-)
+//
+ 
+    int ierr;
+    int idPlate = gmshModelOccAddRectangle(x - w / 2.0, y - h / 2.0, 0, w, h , -1, 0.0,&ierr);   
+    ErrorGmsh(ierr);
+    int idNotch = gmshModelOccAddDisk(x0, y0, 0, r0, r0, -1,NULL,0,NULL,0,&ierr); 
+    ErrorGmsh(ierr);
+    int idHole  = gmshModelOccAddDisk(x1, y1, 0, r1, r1, -1,NULL,0,NULL,0,&ierr);    
+    ErrorGmsh(ierr);
+    
+    int plate[] = {2,idPlate};
+    int notch[] = {2,idNotch};
+    int hole[]  = {2,idHole};
+    gmshModelOccCut(plate,2,notch,2,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
+    ErrorGmsh(ierr);
+    gmshModelOccCut(plate,2,hole,2,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
+    ErrorGmsh(ierr);
+ 
+//
+//  -2- Définition de la fonction callback pour la taille de référence
+//      Synchronisation de OpenCascade avec gmsh
+//      Génération du maillage (avec l'option Mesh.SaveAll :-)
+                  
+   
+    geoSetSizeCallback(geoSize);
+                                  
+    gmshModelOccSynchronize(&ierr);       
+    gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
+    gmshModelMeshGenerate(2, &ierr);  
+       
+//
+//  Generation de quads :-)
+//
+//    gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
+//    gmshOptionSetNumber("Mesh.RecombineAll", 1, &ierr);
+//    gmshOptionSetNumber("Mesh.Algorithm", 8, &ierr);  chk(ierr);
+//    gmshOptionSetNumber("Mesh.RecombinationAlgorithm", 1.0, &ierr);  chk(ierr);
+//    gmshModelGeoMeshSetRecombine(2,1,45,&ierr);  chk(ierr);
+//    gmshModelMeshGenerate(2, &ierr);  
+   
+ 
+//
+//  Plot of Fltk
+//
+//   gmshFltkInitialize(&ierr);
+//   gmshFltkRun(&ierr);  chk(ierr);
+//
+    
+}
---
+++ linux_install.sh
@@ -0,0 +1,2 @@
+sudo apt-get install xorg-dev libglu1-mesa-dev
+sudo apt-get install cmake
