0f0fc0b877efbc3089f534c7e5f1d811c9fdb714
De Raadt Tristan <tristan.deraadt@student.uclouvain.be>
2025-02-14 11:33:05 +0100

    Added Poisson

--- Integrate/src/homework.c
+++ Integrate/src/homework.c
@@ -13,22 +13,19 @@ double integrate(double x[3], double y[3], double (*f) (double, double))
     double xLoc[3];
     double yLoc[3];
 
-    double sixth = 1.0 / 6.0;
-    double twoThirds = 2.0 / 3.0;
-
     //Xloc et Yloc points utilisés pour l'intégration
 
-    xLoc[0] = x[0] * twoThirds + (x[1] + x[2]) * sixth; //  = X0 * 2/3 + X1 * 1/6 + X2 * 1/6 
-    xLoc[1] = (x[0] + x[1]) * sixth + x[2] * twoThirds; //  = X0 * 1/6 + X1 * 1/6 + X2 * 2/3
-    xLoc[2] = (x[0] + x[2]) * sixth + x[1] * twoThirds; //  = X0 * 1/6 + X1 * 2/3 + X2 * 1/6
-    yLoc[0] = y[0] * twoThirds + (y[1] + y[2]) * sixth; //  = Y0 * 2/3 + Y1 * 1/6 + Y2 * 1/6
-    yLoc[1] = (y[0] + y[1]) * sixth + y[2] * twoThirds; //  = Y0 * 1/6 + Y1 * 1/6 + Y2 * 2/3
-    yLoc[2] = (y[0] + y[2]) * sixth + y[1] * twoThirds; //  = Y0 * 1/6 + Y1 * 2/3 + Y2 * 1/6
+    xLoc[0] = x[0] * 0.66667 + (x[1] + x[2]) * 0.16667; //  = X0 * 2/3 + X1 * 1/6 + X2 * 1/6 
+    xLoc[1] = (x[0] + x[1]) * 0.16667 + x[2] * 0.66667; //  = X0 * 1/6 + X1 * 1/6 + X2 * 2/3
+    xLoc[2] = (x[0] + x[2]) * 0.16667 + x[1] * 0.66667; //  = X0 * 1/6 + X1 * 2/3 + X2 * 1/6
+    yLoc[0] = y[0] * 0.66667 + (y[1] + y[2]) * 0.16667; //  = Y0 * 2/3 + Y1 * 1/6 + Y2 * 1/6
+    yLoc[1] = (y[0] + y[1]) * 0.16667 + y[2] * 0.66667; //  = Y0 * 1/6 + Y1 * 1/6 + Y2 * 2/3
+    yLoc[2] = (y[0] + y[2]) * 0.16667 + y[1] * 0.66667; //  = Y0 * 1/6 + Y1 * 2/3 + Y2 * 1/6
 
     for(unsigned short int i = 0; i < 3; ++i)
         I += f(xLoc[i],yLoc[i]);
 
-    I *= sixth * jacobien(x,y); //poids tous égaux à 1/6
+    I *= 0.16667 * jacobien(x,y); //poids tous égaux à 1/6
 
     
     glfemSetColor(GLFEM_BLACK); glfemDrawElement(x,y,3);
@@ -64,17 +61,17 @@ double integrateRecursive(double x[3], double y[3], double (*f)(double,double), 
         //points des milieux des segments
 
 
-        double tr1x[3] = {x[0],x2[0],x2[2]}; 
-        double tr1y[3] = {y[0],y2[0],y2[2]}; 
+        double trx[3] = {x[0],x2[0],x2[2]}; 
+        double try[3] = {y[0],y2[0],y2[2]}; 
         I += integrateRecursive(tr1x,tr1y,f,n - 1);
-        double tr2x[3] = {x2[0],x[1],x2[1]}; 
-        double tr2y[3] = {y2[0],y[1],y2[1]}; 
+        trx[3] = {x2[0],x[1],x2[1]}; 
+        try[3] = {y2[0],y[1],y2[1]}; 
         I += integrateRecursive(tr2x,tr2y,f,n - 1);
-        double tr3x[3] = {x2[2],x2[1],x[2]}; 
-        double tr3y[3] = {y2[2],y2[1],y[2]}; 
+        trx[3] = {x2[2],x2[1],x[2]}; 
+        try[3] = {y2[2],y2[1],y[2]}; 
         I += integrateRecursive(tr3x,tr3y,f,n - 1);
-        double tr4x[3] = {x2[0],x2[1],x2[2]}; 
-        double tr4y[3] = {y2[0],y2[1],y2[2]}; 
+        trx[3] = {x2[0],x2[1],x2[2]}; 
+        try[3] = {y2[0],y2[1],y2[2]}; 
         I += integrateRecursive(tr4x,tr4y,f,n - 1);
 
         //somme des intégrales sur les 4 sous-triangles
